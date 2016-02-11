39cdfb049246db211eb450ab1e4d29f455db251d
alex707 <alex.grigorenko2942@gmail.com>
2016-02-11 14:58:41 +0300

    added design in login, signup and users' index form

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,5 @@ class Project < ActiveRecord::Base
   has_attached_file :project_avatar, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :project_avatar, content_type: /\Aimage\/.*\Z/
 
+  belongs_to :user
 end
